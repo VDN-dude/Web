e897e55b0711d02273ad52cdef4c2364e9ac4d24
Yuri-Vanderon <garanovich2@gmail.com>
2023-04-10 13:34:32 +0300

    deleted date formatter & loop

--- src/main/java/by/tms/servlet/CalcHistoryServlet.java
+++ src/main/java/by/tms/servlet/CalcHistoryServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @WebServlet("/calchistory")
@@ -19,19 +18,9 @@ public class CalcHistoryServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         User user = (User) req.getSession().getAttribute("user");
-        List<Operation> operationList = calculatorService.findUserOperations(user.getUsername());
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss yyyy-MM-dd");
-        StringBuilder operationBuilder = new StringBuilder();
+        List<Operation> operationList = calculatorService.findUserOperations(user.getUserId());
+        req.setAttribute("operationList", operationList);
 
-        for (Operation operation : operationList) {
-            String stringOperation = operation.getNum1() + " " + operation.getType() + " " + operation.getNum2() + " result " + operation.getResult() + " - time: " + operation.getTime().format(formatter);
-            operationBuilder.append(stringOperation);
-            operationBuilder.append("\n");
-        }
-
-        String history = operationBuilder.toString();
-        String[] splitHistory = history.split("\n");
-        req.setAttribute("splitHistory", splitHistory);
         req.getRequestDispatcher("/pages/calchistory.jsp").forward(req, resp);
     }
 
