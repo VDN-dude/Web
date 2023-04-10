fed0e3fb907e606a9cb99ebe026eb512c7eb3c45
Yuri-Vanderon <garanovich2@gmail.com>
2023-04-10 13:45:37 +0300

    added SumOperation class

---
+++ src/main/java/by/tms/service/SumOperation.java
@@ -0,0 +1,33 @@
+package by.tms.service;
+
+import by.tms.entity.Operation;
+import by.tms.entity.OperationType;
+import by.tms.storage.JDBCOperationStorage;
+import by.tms.storage.OperationStorage;
+
+import java.time.LocalDateTime;
+
+public class SumOperation implements CalculatorOperation{
+    private final OperationStorage storage = new JDBCOperationStorage();
+    private final Operation operation;
+
+    public SumOperation(Operation operation) {
+        this.operation = operation;
+    }
+
+    @Override
+    public void process() {
+        operation.setResult(operation.getNum1() + operation.getNum2());
+        operation.setType(OperationType.SUM);
+        operation.setTime(LocalDateTime.now());
+        if (operation.getUserId() != 0){
+            Thread thread = new Thread(() -> storage.save(operation));
+            thread.start();
+        }
+    }
+
+    @Override
+    public double getFinalResult() {
+        return operation.getResult();
+    }
+}
