1a50be3f125c7879990834be10032add48eeb051
Yuri-Vanderon <garanovich2@gmail.com>
2023-04-10 19:18:02 +0300

    added checkEmail $ checkUsername

--- src/main/java/by/tms/storage/UserStorage.java
+++ src/main/java/by/tms/storage/UserStorage.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 public interface UserStorage {
     void save(User user);
     Optional<User> findByEmail(String email);
+    boolean checkEmail(String email);
+    boolean checkUsername(String username);
 }
