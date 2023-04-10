592074b5a10e2895a2dc62763e25654fcf058f14
Yuri-Vanderon <garanovich2@gmail.com>
2023-04-10 13:44:49 +0300

    changed operationType to upper case

--- src/main/java/by/tms/validator/CalculatorValidator.java
+++ src/main/java/by/tms/validator/CalculatorValidator.java
@@ -3,9 +3,9 @@ package by.tms.validator;
 import java.util.regex.Pattern;
 
 public class CalculatorValidator {
-    private static final Pattern NUM = Pattern.compile("^\\d[.\\d]*$");
-    private static final Pattern OperationType = Pattern.compile("^(SUM)\\b|(SUB)\\b|(MUL)\\b|(DIV)\\b$");
+    private static final Pattern NUM = Pattern.compile("^\\d[.\\d]*");
+    private static final Pattern OPERATION_TYPE = Pattern.compile("^(SUM)\\b|(SUB)\\b|(MUL)\\b|(DIV)\\b");
 
     public boolean isValidNum(String num){return NUM.matcher(num).matches();}
-    public boolean isValidOperationType(String type){return OperationType.matcher(type).matches();}
+    public boolean isValidOperationType(String type){return OPERATION_TYPE.matcher(type).matches();}
 }
