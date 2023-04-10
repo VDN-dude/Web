9e9e6166a9a84e0bd30765a51646f22082f4fb8f
Yuri-Vanderon <garanovich2@gmail.com>
2023-04-10 13:50:24 +0300

    made class extends from JDBCUserStorage

--- src/main/java/by/tms/service/UserService.java
+++ src/main/java/by/tms/service/UserService.java
@@ -2,14 +2,25 @@ package by.tms.service;
 
 import by.tms.entity.User;
 import by.tms.storage.JDBCUserStorage;
+import by.tms.storage.UserStorage;
 
-public class UserService {
-    private final JDBCUserStorage storage = new JDBCUserStorage();
+public class UserService extends JDBCUserStorage{
+    private final UserStorage storage = new JDBCUserStorage();
 
     public void save(User user){
         storage.save(user);
     }
     public java.util.Optional<User> findByEmail(String email){
         return storage.findByEmail(email);
+    }
+
+    @Override
+    public boolean checkEmail(String email) {
+        return super.checkEmail(email);
+    }
+
+    @Override
+    public boolean checkUsername(String username) {
+        return super.checkUsername(username);
     }
 }
