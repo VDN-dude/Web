62aed1beba04a2dc32e700f216e5205313a54a74
Yuri-Vanderon <garanovich2@gmail.com>
2023-04-10 13:49:38 +0300

    changed id to userId

--- src/main/java/by/tms/entity/User.java
+++ src/main/java/by/tms/entity/User.java
@@ -1,12 +1,12 @@
 package by.tms.entity;
 
 public class User {
-    private int id;
-    private String firstName;
-    private String lastName;
-    private String username;
-    private String email;
-    private String password;
+    private int userId;
+    private final String firstName;
+    private final String lastName;
+    private final String username;
+    private final String email;
+    private final String password;
 
     public User(String firstName, String lastName, String username, String email, String password) {
         this.firstName = firstName;
@@ -16,8 +16,8 @@ public class User {
         this.password = password;
     }
 
-    public User(int id, String firstName, String lastName, String username, String email, String password) {
-        this.id = id;
+    public User(int userId, String firstName, String lastName, String username, String email, String password) {
+        this.userId = userId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.username = username;
@@ -25,58 +25,34 @@ public class User {
         this.password = password;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+    public int getUserId() {
+        return userId;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public String getEmail() {
+        return email;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     @Override
     public String toString() {
         return "User{" +
-                "id=" + id +
+                "userId='" + userId + '\'' +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", username='" + username + '\'' +
