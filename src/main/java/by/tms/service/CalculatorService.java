299c7eda28df7e70888ac7e8e654f4ee0ab97978
Yuri-Vanderon <garanovich2@gmail.com>
2023-04-12 14:11:57 +0300

    changed method calculate & added method configure

--- src/main/java/by/tms/service/CalculatorService.java
+++ src/main/java/by/tms/service/CalculatorService.java
@@ -2,29 +2,60 @@ package by.tms.service;
 
 
 import by.tms.entity.Operation;
+import by.tms.factory.*;
 import by.tms.storage.JDBCOperationStorage;
 import by.tms.storage.OperationStorage;
 
 import java.util.List;
 
 public class CalculatorService {
-   private static CalculatorService instance;
-   private final OperationStorage storage = JDBCOperationStorage.getInstance();
-   private CalculatorService(){
+    private Operation operation;
+    private static OperationFactory factory;
+    private static CalculatorService instance;
+    private final OperationStorage storage = JDBCOperationStorage.getInstance();
 
-   }
-   public static CalculatorService getInstance(){
-      if (instance == null){
-         instance = new CalculatorService();
-      }
-      return instance;
-   }
-   public double calculate(CalculatorOperation operation){
-      operation.process();
-      return operation.getFinalResult();
-   }
-   public List<Operation> findByUserId(int userId) {
-      return storage.findByUserId(userId);
-   }
+    private CalculatorService() {
+
+    }
+
+    public static CalculatorService getInstance() {
+        if (instance == null) {
+            instance = new CalculatorService();
+        }
+        return instance;
+    }
+
+    public void configure(Operation operation) {
+        switch (operation.getType()) {
+            case SUM:
+                factory = new SumOp();
+                this.operation = operation;
+                break;
+            case SUB:
+                factory = new SubOp();
+                this.operation = operation;
+                break;
+            case MUL:
+                factory = new MulOp();
+                this.operation = operation;
+                break;
+            case DIV:
+                factory = new DivOp();
+                this.operation = operation;
+                break;
+        }
+
+    }
+    public Operation calculate() {
+        if (factory != null) {
+            factory.executeOperation(operation);
+            return operation;
+        }
+        return null;
+    }
+
+    public List<Operation> findByUserId(int userId) {
+        return storage.findByUserId(userId);
+    }
 
 }
