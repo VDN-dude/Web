f6b4cb5afdcd5010855aa6d5ec4a4e59b38288b3
Yuri-Vanderon <garanovich2@gmail.com>
2023-04-19 19:34:55 +0300

    changed int userId to User user

--- src/main/java/by/tms/servlet/CalcServlet.java
+++ src/main/java/by/tms/servlet/CalcServlet.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Optional;
 
 @WebServlet(name = "CalcServlet", urlPatterns = "/calc")
 public class CalcServlet extends HttpServlet {
